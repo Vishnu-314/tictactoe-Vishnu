e454797e7e65ffdd0e6486319cb1519e9733e119
Vishnu-314 <vishnu846684@gmail.com>
2023-08-07 15:57:47 +0530

    added sass, created basic layout of board and square components

---
+++ src/components/Squre.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+const Squre = ({value}) => {
+  return (
+    <button className="square">
+      {value}
+    </button>
+  )
+}
+
+export default Squre
